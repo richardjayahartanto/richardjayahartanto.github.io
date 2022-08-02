9d9f96f3cfa3e1fda3f1c68ee19f46205bbe4421
Richard Jaya Hartanto <richardjayahartanto@gmail.com>
2022-08-02 11:43:04 +0700

    change index.php to index.html

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,9 @@ let $filter = $('.filter-element').isotope({
     filter: '.websites'
 })
 
-$('.filter-button-group').on('click', 'a', function() {
+$('.filter-button-group').on('click', 'a', function () {
     $('.filter-button-group').find('a').removeClass('active')
     let filterValue = $(this).addClass('active').attr('data-filter')
-    $filter.isotope({ filter: filterValue })
+    $filter.isotope({filter: filterValue})
 });
+
