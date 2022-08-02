a27e3741df5af7fe2b5f7a6c7079a40d5e919971
Richard Jaya Hartanto <richardjayahartanto@gmail.com>
2022-08-02 12:18:23 +0700

    add experience date using moment

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,19 @@
-let $filter = $('.filter-element').isotope({
-    filter: '.websites'
+$(document).ready(function () {
+    let $filter = $('.filter-element').isotope({
+        filter: '.websites'
+    })
+
+    $('.filter-button-group').on('click', 'a', function () {
+        $('.filter-button-group').find('a').removeClass('active')
+        let filterValue = $(this).addClass('active').attr('data-filter')
+        $filter.isotope({filter: filterValue})
+    });
+
+    const date1 = moment("2011-11-01")
+    const date2 = moment("2018-09-07")
+
+    $('#experience_date1').html(date1.format('MMM YYYY'))
+    $('#experience_date2').html(date2.format('MMM YYYY'))
+    $('#experience_calc1').html(date1.toNow(true))
+    $('#experience_calc2').html(date2.toNow(true))
 })
-
-$('.filter-button-group').on('click', 'a', function () {
-    $('.filter-button-group').find('a').removeClass('active')
-    let filterValue = $(this).addClass('active').attr('data-filter')
-    $filter.isotope({filter: filterValue})
-});
-
