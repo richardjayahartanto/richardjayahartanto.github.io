601f03aedb3b4ecf26532332787c30a8f11a746c
Richard Jaya Hartanto <richardjayahartanto@gmail.com>
2023-04-28 10:37:52 +0700

    update experience & resume

--- js/custom.js
+++ js/custom.js
@@ -2,18 +2,18 @@ $(document).ready(function () {
     let $filter = $('.filter-element').isotope({
         filter: '.websites'
     })
-
+    
     $('.filter-button-group').on('click', 'a', function () {
         $('.filter-button-group').find('a').removeClass('active')
         let filterValue = $(this).addClass('active').attr('data-filter')
         $filter.isotope({filter: filterValue})
     });
-
+    
     const date1 = moment("2011-11-01")
     const date2 = moment("2018-09-07")
     
     $('#experience_date1').html(date1.format('MMM YYYY'))
     $('#experience_date2').html(date2.format('MMM YYYY'))
-    $('#experience_calc1').html(date1.toNow(true))
+    $('#experience_calc1').html("11 years")
     $('#experience_calc2').html(date2.toNow(true))
 })
