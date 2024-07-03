d7b2adaad93a75bb5a672ce6cfdfd006e1f92560
Richard Jaya Hartanto <Richardjayahartanto@gmail.com>
2024-07-03 17:39:45 +0700

    update experience & resume

--- js/custom.js
+++ js/custom.js
@@ -11,9 +11,12 @@ $(document).ready(function () {
     
     const date1 = moment("2011-11-01")
     const date2 = moment("2018-09-07")
+    const date3 = moment("2023-05-01")
     
     $('#experience_date1').html(date1.format('MMM YYYY'))
     $('#experience_date2').html(date2.format('MMM YYYY'))
+    $('#experience_date3').html(date3.format('MMM YYYY'))
     $('#experience_calc1').html("11 years")
     $('#experience_calc2').html(date2.toNow(true))
+    $('#experience_calc3').html(date3.toNow(true))
 })
